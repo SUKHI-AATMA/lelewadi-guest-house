90dce6afb7ba9e23339a2ab9216c9b5eb0b5b5af
Rajesh Sawant <sawant1810@gmail.com>
2024-03-16 22:27:06 +0530

    updated page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    theme: {
+        colors: {
+            secondary: colors.indigo,
+            primary: colors.yellow,
+            neutral: colors.gray,
+        }
+    }
+}
